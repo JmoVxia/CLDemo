2be35d39fa72676da1909a79febc3938a0e8afce
JmoVxia <610934716@qq.com>
2016-12-16 16:42:50 +0800

    增加自定义搜索框

---
+++ CLDemo/Class/Base/CLSearchBar.h
@@ -0,0 +1,13 @@
+//
+//  CLSearchBar.h
+//  AISchoolDiscipline
+//
+//  Created by JmoVxia on 2016/12/10.
+//  Copyright © 2016年 艾泽鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CLSearchBar : UITextField
+
+@end
