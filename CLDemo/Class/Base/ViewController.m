7a0e2c45a094063aa31ba399e18f0e2850339456
JmoVxia <269968846@qq.com>
2017-03-09 17:25:14 +0800

    还原

--- CLDemo/Class/Base/ViewController.m
+++ CLDemo/Class/Base/ViewController.m
@@ -16,7 +16,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.view.backgroundColor = [UIColor whiteColor];
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
--- CLDemo/Class/Me/Controller/MeController.m
+++ CLDemo/Class/Me/Controller/MeController.m
@@ -10,81 +10,27 @@
 
 #import "MeController.h"
 
-@interface MeController ()<UITableViewDataSource,UITableViewDelegate>
-
-/**tableView*/
-@property (nonatomic,strong) UITableView *tableView;
-/**数据源*/
-@property (nonatomic,strong) NSMutableArray *arrayDS;
-
+@interface MeController ()
 
 @end
 
 @implementation MeController
 
-
-- (UITableView *) tableView{
-    if (_tableView == nil){
-        _tableView = [[UITableView alloc] init];
-        _tableView.delegate = self;
-        _tableView.dataSource = self;
-        _tableView.showsHorizontalScrollIndicator = NO;
-    }
-    return _tableView;
-}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.arrayDS = [NSMutableArray array];
+    
     self.view.backgroundColor = [UIColor greenColor];
-    self.navigationItem.title = @"朋友圈";
-    
-    
-    
+    self.navigationItem.title = @"我的";
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return _arrayDS.count;
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
 }
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
-    static NSString *Identifier = @"aaa";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:Identifier];
-    if (!cell)
-    {
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:Identifier];
-    }
-    [cell.contentView.subviews makeObjectsPerformSelector:@selector(removeFromSuperview)];
-    
-    return cell;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+*/
 
 @end
