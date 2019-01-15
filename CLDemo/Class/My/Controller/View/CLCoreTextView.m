c6153002864ae6dd18987e4a80811e8be7f28e2f
JmoVxia <269968846@qq.com>
2019-01-16 01:52:39 +0800

    增加计算高度方法

--- CLDemo/Class/My/Controller/View/CLCoreTextView.m
+++ CLDemo/Class/My/Controller/View/CLCoreTextView.m
@@ -7,6 +7,7 @@
 //
 
 #import "CLCoreTextView.h"
+#import "UIView+CLSetRect.h"
 #import <CoreText/CoreText.h>
 
 @interface CLCoreTextView ()
@@ -305,9 +306,44 @@ static CGFloat widthCallBacks(void *ref)
     return NO;
 }
 
+- (int)getAttributedStringHeightWithString:(NSAttributedString *)string  maxWidth:(CGFloat)maxWidth
+{
+    int total_height = 0;
+    
+    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString((CFAttributedStringRef)string);    //string 为要计算高度的NSAttributedString
+    CGRect drawingRect = CGRectMake(0, 0, maxWidth, 1000);  //这里的高要设置足够大
+    CGMutablePathRef path = CGPathCreateMutable();
+    CGPathAddRect(path, NULL, drawingRect);
+    CTFrameRef textFrame = CTFramesetterCreateFrame(framesetter,CFRangeMake(0,0), path, NULL);
+    CGPathRelease(path);
+    CFRelease(framesetter);
+    
+    NSArray *linesArray = (NSArray *) CTFrameGetLines(textFrame);
+    
+    CGPoint origins[[linesArray count]];
+    CTFrameGetLineOrigins(textFrame, CFRangeMake(0, 0), origins);
+    
+    int line_y = (int) origins[[linesArray count] -1].y;  //最后一行line的原点y坐标
+    
+    CGFloat ascent;
+    CGFloat descent;
+    CGFloat leading;
+    
+    CTLineRef line = (CTLineRef) CFBridgingRetain([linesArray objectAtIndex:[linesArray count] - 1]);
+    CTLineGetTypographicBounds(line, &ascent, &descent, &leading);
+    
+    total_height = 1000 - line_y + (int) descent +1;    //+1为了纠正descent转换成int小数点后舍去的值
+    
+    CFRelease(textFrame);
+    
+    return total_height;
+    
+}
 - (void)dealloc
 {
-    CFRelease(_ctFrm);
+    if (_ctFrm) {
+        CFRelease(_ctFrm);
+    }
 }
 
 
